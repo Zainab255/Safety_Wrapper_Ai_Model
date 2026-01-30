4c4fe4fd500ac6a019e5e7c01abcee36eecee196
Zainab Ashraf <jizainab204@gmail.com>
2026-01-31 03:53:33 +0500

    Integrated HuggingFace_Model

--- models/llm_client.py
+++ models/llm_client.py
@@ -2,7 +2,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 class BlackBoxLLM:
-    def __init__(self, model_name: str = "gpt2", max_length: int = 30):
+    def __init__(self, model_name: str = "gpt2", max_length: int = 25):
       
         self.model_name = model_name
         self.max_length = max_length
